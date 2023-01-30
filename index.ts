49c707ec574ab8cc2d9058901988df01e7bd489e
Nick <nick.foose27@gmail.com>
2023-01-30 17:38:13 -0500

    rm index.ts file, not needed

--- index.ts
+++
@@ -1,6 +0,0 @@
-const display = document.getElementById('display')?.innerHTML;
-const numeralButton: string = document.querySelectorAll(".numeral");
-
-numeralButton.addEventListener("click", () => {
-    console.log('itworks')
-});
